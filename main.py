1730dcab50450fc703adc25d3a31fe426ff5ef7c
Aef-Watcharakorn <Watcharakorn.aef@g.swu.ac.th>
2021-03-07 22:58:21 +0700

    พยายามเพิ่มหน้าข่าว+เพิ่มรูป

--- main.py
+++ main.py
@@ -5,25 +5,23 @@ from flask import request
 from urllib.parse import quote
 from urllib.request import urlopen
 import json
+import os
 
 main = Blueprint('main', __name__)
 
-OPEN_COVID19_URL = "http://newsapi.org/v2/everything?q={0}&language=en&sortBy=publishedAt&pageSize=5&apiKey={1}"
-OPEN_COVID19_KEY = '0f123f4c5b4f45a6b33d7ca370bbaf05'
+FINNHUB_API_KEY = os.getenv('FINNHUB_API_KEY','demo')
+OPEN_FINNHUB_NEWS_URL = "https://finnhub.io/api/v1/news?category={0}&token={1}"
+API_KEY = 'c0sit9n48v6tv6b8fm70'
 
-OPEN_NEWS_URL = "http://newsapi.org/v2/everything?q={0}&language=en&sortBy=publishedAt&pageSize=5&apiKey={1}"
 
 @main.route("/")
 def index():
     title = "Home"
     news = request.args.get('news')
     if not news:
-        news = 'covid-19'
-
-    covid19 = get_covid19(news, OPEN_COVID19_KEY)
-    
-
-    return render_template("index.html", title=title, covid19=covid19, news=news)
+        news = 'general'
+    general = get_news(news, FINNHUB_API_KEY)
+    return render_template("index.html", title=title, general=general)
 
 
 @main.route("/aboutus")
@@ -36,21 +34,13 @@ def about():
 def serve_img(img_id):
     pass
 
-def get_covid19(news,API_KEY):
-    queryNews = quote(news)
-    url = OPEN_COVID19_URL.format(queryNews, OPEN_COVID19_KEY)
+def get_news(news, API_KEY):
+    query = quote(news)
+    url = OPEN_FINNHUB_NEWS_URL.format(query, API_KEY)
     data = urlopen(url).read()
     parsed = json.loads(data)
 
-    covid19 = parsed.get('articles')
-    
-    return covid19
+    News = parsed
+    return News
 
-def get_searchNews(news, API_KEY):
-    queryNews = quote(news)
-    url = OPEN_NEWS_URL.format(queryNews, API_KEY)
-    data = urlopen(url).read()
-    parsed = json.loads(data)
-    news = parsed.get('articles')
-    return news
 
