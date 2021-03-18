4361173654175a6fc9b098dc0b8aecb57a1967cc
porntavee-ball <porntavee.ball@g.swu.ac.th>
2021-03-18 20:47:29 +0700

    aboutus

--- models.py
+++ models.py
@@ -7,4 +7,6 @@ class Developer(db.Model):        # Class User สืบทอดมาจาก
     ID = db.Column(db.Integer, primary_key=True) 
     Name = db.Column(db.String(100))
     Std_id = db.Column(db.Integer)
-    Picture = db.Column(db.BLOB)
+    Picture = db.Column(db.String(100))
+    Facebook = db.Column(db.String(100))
+    IG = db.Column(db.String(100))
